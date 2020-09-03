f14e1e5b633a875e265fd7a56548bb8ac99f2b55
norikatsu111 <62043831+norikatsu111@users.noreply.github.com>
2020-09-03 12:25:07 +0900

    first commit

---
+++ text_counter.py
@@ -0,0 +1,24 @@
+# tkinterをインポート
+from tkinter import *
+
+# テキストの文字列を数える関数
+def count_text(event):
+	s = main_text.get(1.0, END) # テキストの最初から最後までを取得
+	info_label.config(text = "{0}文字".format(len(s)))
+	
+# メインウィンドウを作成
+root = Tk()
+root.title('テキストカウンタ')
+
+# テキストボックスを作成
+main_text = Text(root)
+main_text.bind("<Key>", count_text) # イベントを設定
+main_text.pack()
+
+# 文字列を表示するラベルを作成
+info_label = Label(root)
+info_label.pack()
+
+# メインループ
+root.mainloop()
+
